faf8356223ecd06c1e099462f5b39df1865b1563
Chris Laskey <contact@chrislaskey.com>
2014-09-29 10:58:09 -0400

    Added footer

---
+++ wordpress-theme/footer.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage CELEST Responsive
+ * @since 3.0.0
+ */
+?>
+
+    </div>
+
+    <div class="row" id="main-bottom">
+        
+    </div>
+
+    <div class="row" id="sponsors">
+        <div class="large-12 columns">
+            <h4>Conference Sponsors</h4>
+            <div class="row">
+                <div class="large-3 columns sponsor-container">
+                    <div class="sponsor">
+                        <a href="http://celest.bu.edu" target="blank">
+                            <img alt="CELEST" src="<?php echo CL_TEMPLATEPATH; ?>/img/sponsor-celest.png"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="large-3 columns sponsor-container">
+                    <div class="sponsor">
+                        <a href="http://compnet.bu.edu" target="blank">
+                            <img alt="CompNet" src="<?php echo CL_TEMPLATEPATH; ?>/img/sponsor-compnet.png"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="large-3 columns sponsor-container">
+                    <div class="sponsor">
+                        <a href="http://nsf.gov" target="blank">
+                            <img alt="NSF" src="<?php echo CL_TEMPLATEPATH; ?>/img/sponsor-nsf.png"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="large-3 columns sponsor-container">
+                    <div class="sponsor">
+                        <a href="http://www.bu.edu" target="blank">
+                            <img alt="Boston University" src="<?php echo CL_TEMPLATEPATH; ?>/img/sponsor-bu.png"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="<?php echo CL_TEMPLATEPATH; ?>/js/vendor/jquery.js"></script>
+    <script src="<?php echo CL_TEMPLATEPATH; ?>/js/foundation.min.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-11681368-6', 'auto');
+        ga('send', 'pageview');
+    </script>
+
+</body>
+</html>
